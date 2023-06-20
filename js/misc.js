58fd22f051e3cb8d982bbbfcb4fbe54ea784ee0c
Abhik555 <ghoshabhik555@gmail.com>
2023-06-21 00:19:26 +0530

    Added Suggestions box

--- js/misc.js
+++ js/misc.js
@@ -1,7 +1,14 @@
-const btn = document.getElementsByClassName("github");
-
 function opengithub(){
-    console.log("Opening Github")
-    console.log(btn.item.toString)
-    window.open("https://www.github.com/Abhik555")
+    console.log("Opening Github");
+    console.log(btn.item.toString);
+    window.open("https://www.github.com/Abhik555");
+}
+
+function submitsuggestion(){
+    const text = document.getElementById("text1");
+    const name = document.getElementById("email");
+    const idea = text.value;
+    const author = name.value;
+    console.log(idea)
+    console.log(author)
 }
