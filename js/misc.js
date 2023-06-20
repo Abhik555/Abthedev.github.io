9fcbf0667c7355c5a5ccff6bc4e59b2ceb27ac35
Abhik555 <ghoshabhik555@gmail.com>
2023-06-20 23:01:22 +0530

    About Page work half way point

---
+++ js/misc.js
@@ -0,0 +1,7 @@
+const btn = document.getElementsByClassName("github");
+
+function opengithub(){
+    console.log("Opening Github")
+    console.log(btn.item.toString)
+    window.open("https://www.github.com/Abhik555")
+}
